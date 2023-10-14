a6f4dff18325cca727f400c7390b5ec82b24756e
nicolyjjang <nicolyjjang@gmail.com>
2023-10-14 16:32:33 -0300

    teste

--- dio-partidas-rankeadas/index.js
+++ dio-partidas-rankeadas/index.js
@@ -26,3 +26,36 @@ Ao final deve se exibir uma mensagem:
 "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
  
 */
+let vitorias = 15;
+let derrotas = 5;
+let rank;
+
+const saldoVitorias = vitorias - derrotas;
+
+function saldoRankeadas(vitorias, derrotas){
+    if(vitorias <10){
+        rank = "Ferro" 
+    }
+    else if(vitorias >= 11 && vitorias >= 20){
+        rank = "Bronze"
+    }
+    else if(vitorias >= 21 && vitorias >= 50){
+        rank = "Prata"
+    }
+    else if(vitorias >= 51 && vitorias >= 80){
+        rank = "Ouro"
+    }
+    else if(vitorias >= 81 && vitorias >= 90){
+        rank = "Diamante"
+    }
+    else if(vitorias >= 91 && vitorias >= 100){
+        rank = "Lendário"
+    }
+    else if(vitorias >= 101){
+        rank = "Imortal"
+    }
+    
+
+    return saldoVitorias;
+}
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${rank}`)
